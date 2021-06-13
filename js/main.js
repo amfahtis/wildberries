9bdcb8303b065b5d387f609a66dda4cf22c4ab2b
Saidakbar <30874817+amfahtis@users.noreply.github.com>
2021-06-13 18:56:25 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,49 @@
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-button-next',
-      prevEl: '.slider-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
 
-  const swiperPlay = new Swiper('.swiper-container', {
-    autoplay: {
-      delay: 5000,
-    },
-   });
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+
+// Cart
+
+/* 
+
+*/
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+
+const openModal = function() {
+	modalCart.classList.add('show')
+};
+
+const closeModal = function() {
+	modalCart.classList.remove('show')
+};
+
+buttonCart.addEventListener('click', openModal);
+
+modalClose.addEventListener('click', closeModal);
+
+// Scroll Smooth Script = SSS
+
+(function() {
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++){
+	scrollLinks[i].addEventListener('click', function(event) {
+		event.preventDefault();
+		const id = scrollLinks[i].getAttribute('href');
+		document.querySelector(id).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	});
+}
+})()
